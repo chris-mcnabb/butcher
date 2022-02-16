8863c218650eb254d80f77adde4fa4294d4069f2
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2022-02-16 19:44:14 +0100

    corrected gt

--- pages/api/auth/getToken.js
+++ pages/api/auth/getToken.js
@@ -4,7 +4,7 @@ import { getToken } from "next-auth/jwt"
 const secret = process.env.NEXTAUTH_SECRET
 
 
-export default async (req, res)  {
+export default async function(req, res)  {
     const token = await getToken({req, secret})
     console.log("JSON Web Token", token)
     res.end()
