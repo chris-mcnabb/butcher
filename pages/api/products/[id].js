8adcb23e1dbac31924ad434f07805eaae715819f
Chris <75439614+chris-mcnabb@users.noreply.github.com>
2022-03-10 14:16:21 +0100

    photos and uploads

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -12,6 +12,7 @@ export default async function handler(req, res) {
     if(method==="GET"){
         try{
             const product = await Product.findById(id);
+            res.setHeader('Cache-Control', 's-maxage=600, stale-while-revalidate');
             res.status(200).json(product)
         }catch(err){
             res.status(500).json(err)
